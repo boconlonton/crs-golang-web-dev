2d5bf9204aedf009d19f28d758b78f7dd3c1334e
bocon <tan.truong@go2joy.vn>
2021-12-09 17:38:43 +0700

    Add - 004_net-http-package

---
+++ 004_net-http-package/01_listenAndServe/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type hotdog int
+
+func (m hotdog) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Any code you want in this func")
+}
+
+func main() {
+	var d hotdog
+	err := http.ListenAndServe(":8080", d)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
